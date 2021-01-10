9e87c8dfba3d797e804cf1944556b74563ab1a1a
IshanAhuja13 <76620278+IshanAhuja13@users.noreply.github.com>
2021-01-10 18:03:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  gameObject1 = createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor = "red";
+  gameObject3.velocityX = 3;
+  gameObject4 = createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor = "green";
+  gameObject4.velocityX = -3;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "blue";
+    gameObject1.shapeColor = "blue";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
+  }
+
+  if(bounceOff(gameObject3, gameObject4)){
+    gameObject3.velocityX = gameObject3.velocityX * (-1);
+    gameObject4.velocityX = gameObject4.velocityX * (-1);
+  }
+
+  else{
+    gameObject3.velocityY = gameObject3.velocityY * (-1);
+    gameObject4.velocityY = gameObject4.velocityY * (-1);
+  }
+
+  console.log(gameObject3.x);
+  console.log(gameObject4.x);
+
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+ 
+  return true;
+ 
+}
+if (object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+  
+  return false;
+}
+}
